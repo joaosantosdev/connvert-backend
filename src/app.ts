173da55e37b10a0d6b77e4e062bc7625face3a8d
Your Name <you@example.com>
2021-03-11 23:12:37 -0300

    initial config

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import * as dotenv from 'dotenv'
+import routes from './routers/Router'
+dotenv.config()
+
+class App {
+    private express: express.Application;
+
+    public constructor () {
+      this.express = express()
+      this.middlewares()
+      this.database()
+    }
+
+    private middlewares (): void {
+      this.express.use(express.json())
+      this.express.use(cors())
+      this.express.use(routes)
+    }
+
+    private database (): void {
+      mongoose.connect(process.env.MONGO_URL_DB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      })
+    }
+
+    public getApp () {
+      return this.express
+    }
+}
+export default new App().getApp()
